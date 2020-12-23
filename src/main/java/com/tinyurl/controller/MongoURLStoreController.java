8f93ca137ca0febf217ce2d3a6bb8b8166276317
CharanNK <charkeet@gmail.com>
2020-12-23 17:54:24 +0530

    closes #10. crud operations with mongodb

--- src/main/java/com/tinyurl/controller/MongoURLStoreController.java
+++ src/main/java/com/tinyurl/controller/MongoURLStoreController.java
@@ -3,8 +3,11 @@ package com.tinyurl.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tinyurl.model.MongoURLStore;
@@ -19,7 +22,51 @@ public class MongoURLStoreController {
 	MongoURLStoreService mongoUrlStoreService;
 	
 	@RequestMapping(value = "/get-all-urls", method = RequestMethod.GET)
-	public List<MongoURLStore> getAllUrls(){
-		return mongoUrlStoreService.getAllUrls();
+	public ResponseEntity<List<MongoURLStore>> getAllUrls(){
+		List<MongoURLStore> listOfUrls = mongoUrlStoreService.getAllUrls();
+		if(listOfUrls.isEmpty())
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		else
+			return new ResponseEntity<>(listOfUrls,HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "/find-short-url", method = RequestMethod.GET)
+	public ResponseEntity<String> getShortUrl(@RequestParam(value = "baseUrl", required = true) String baseUrl){
+		String shortUrl = mongoUrlStoreService.getShortUrl(baseUrl);
+		
+		if(shortUrl==null)
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		else
+			return new ResponseEntity<>(shortUrl,HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "/find-base-url", method = RequestMethod.GET)
+	public ResponseEntity<String> getBaseUrl(@RequestParam(value = "shortUrl", required = true) String shortUrl){
+		String baseUrl = mongoUrlStoreService.getBaseUrl(shortUrl);
+		
+		if(baseUrl == null)
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		else
+			return new ResponseEntity<>(baseUrl,HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "/add-new-url", method = RequestMethod.PUT)
+	public ResponseEntity<MongoURLStore> createNewEntry(@RequestParam(value = "baseUrl",required = true) String baseUrl){
+		MongoURLStore newEntry = mongoUrlStoreService.createUrlEntry(baseUrl);
+		
+		if(newEntry==null){
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		}else
+			return new ResponseEntity<>(newEntry,HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "/delete-by-base-url", method = RequestMethod.DELETE)
+	public ResponseEntity<String> deleteByBaseUrl(@RequestParam(value = "baseUrl",required = true) String baseUrl){
+		boolean isEntryDeleted = mongoUrlStoreService.deleteUrlEntry(baseUrl);
+		
+		if(isEntryDeleted)
+			return new ResponseEntity<>("Url entry deleted!",HttpStatus.OK);
+		else
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	}
 }
--- src/main/java/com/tinyurl/service/MongoURLStoreService.java
+++ src/main/java/com/tinyurl/service/MongoURLStoreService.java
@@ -3,15 +3,19 @@ package com.tinyurl.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 
 import com.tinyurl.model.MongoURLStore;
+import com.tinyurl.util.TinyurlUtil;
 
 import java.util.*;
 
 @Service
 public class MongoURLStoreService {
+	
+	private long index = 9837347L;
 	
 	@Autowired
 	MongoTemplate mongoTemplate;
@@ -21,5 +25,57 @@ public class MongoURLStoreService {
 		return mongoTemplate.find(query, MongoURLStore.class);
 	}
 	
+	public String getBaseUrl(String shortUrl) {
+		
+		if(!TinyurlUtil.isValidURL(shortUrl))
+			return null;
+		
+		Query query = new Query().addCriteria(Criteria.where("shortUrl").is(shortUrl));
+		MongoURLStore urlEntry = mongoTemplate.findOne(query, MongoURLStore.class);
+		
+		return urlEntry.getBaseUrl();
+	}
 	
+	public String getShortUrl(String baseUrl) {
+		
+		if(!TinyurlUtil.isValidURL(baseUrl))
+			return null;
+		
+		Query query = new Query().addCriteria(Criteria.where("baseUrl").is(baseUrl));
+		MongoURLStore urlEntry = mongoTemplate.findOne(query, MongoURLStore.class);
+		
+		return urlEntry.getShortUrl();
+	}
+	
+	public MongoURLStore createUrlEntry(String baseUrl) {
+		if(!TinyurlUtil.isValidURL(baseUrl))
+			return null;
+		
+		Query query = new Query().addCriteria(Criteria.where("baseUrl").is(baseUrl));
+		MongoURLStore urlEntry = mongoTemplate.findOne(query, MongoURLStore.class);
+		
+		if(urlEntry!=null)
+			return urlEntry;
+		else
+			urlEntry = new MongoURLStore(++index, baseUrl, TinyurlUtil.generateShortURL(baseUrl));
+		
+		mongoTemplate.save(urlEntry);
+		
+		return urlEntry;
+	}
+	
+	public boolean deleteUrlEntry(String baseUrl) {
+		if(!TinyurlUtil.isValidURL(baseUrl))
+			return false;
+		
+		Query query = new Query().addCriteria(Criteria.where("baseUrl").is(baseUrl));
+		MongoURLStore urlEntry = mongoTemplate.findOne(query, MongoURLStore.class);
+		
+		if(urlEntry!=null) {
+			mongoTemplate.remove(urlEntry);
+			return true;
+		}else {
+			return false;
+		}
+	}
 }
